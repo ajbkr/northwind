92b9f3b2f781a10ea925e4de4db8c0fe27961f43
Andrew Baker <andrew_j_baker2@hotmail.com>
2019-02-22 14:50:58 +0000

    Lint JS using standard

--- che.conf.js
+++ che.conf.js
@@ -1,54 +1,54 @@
 var helpers = Object.assign({}, require('handlebars-helpers')(), {
   // Convert MySQL data type to equivalent Django/Sequelize/SQLite data type
-  dataType: function(dbms, dataType) {
+  dataType: function (dbms, dataType) {
     var dataTypes = {
       django: {
-        BLOB:     'Binary',
+        BLOB: 'Binary',
         DATETIME: 'Date',
-        DECIMAL:  'Decimal',
-        DOUBLE:   'Float',
-        INT:      'Integer',
+        DECIMAL: 'Decimal',
+        DOUBLE: 'Float',
+        INT: 'Integer',
         LONGBLOB: 'Binary',
         LONGTEXT: 'Text',
-        VARCHAR:  'Char'
+        VARCHAR: 'Char'
       },
       sequelize: {
         DATETIME: 'DATE',
-        INT:      'INTEGER',
+        INT: 'INTEGER',
         LONGBLOB: 'BLOB',
         LONGTEXT: 'TEXT',
-        VARCHAR:  'STRING'
+        VARCHAR: 'STRING'
       },
       sqlite: {
         DATETIME: 'REAL',
-        DECIMAL:  'REAL',
-        DOUBLE:   'REAL',
+        DECIMAL: 'REAL',
+        DOUBLE: 'REAL',
         LONGBLOB: 'BLOB',
         LONGTEXT: 'TEXT',
-        VARCHAR:  'TEXT'
+        VARCHAR: 'TEXT'
       }
-    };
+    }
 
-    return dataTypes && dataTypes[dbms] && dataTypes[dbms][dataType] ?
-     dataTypes[dbms][dataType] :
-     dataType;
+    return dataTypes && dataTypes[dbms] && dataTypes[dbms][dataType]
+      ? dataTypes[dbms][dataType]
+      : dataType
   }
-});
+})
 
-var schema = require('./northwind.json');
+var schema = require('./northwind.json')
 
-var generators = Object.keys(schema.tables).reduce(function(objekt, table) {
+var generators = Object.keys(schema.tables).reduce(function (objekt, table) {
   objekt['/tables/' + table] = {
     'templates/create-table.handlebars': 'dist/' + table.replace(/_/g, '-') + '.js',
     'templates/django-model.handlebars': 'dist/models/' + table + '.py',
     'templates/drf-serializer.handlebars': 'dist/serializers/' + table + '.py',
     'templates/sequelize-model.handlebars': 'dist/models/' + table.replace(/_/g, '-') + '.js'
-  };
-  return objekt;
-}, {});
+  }
+  return objekt
+}, {})
 
 module.exports = {
   generators: generators,
-  helpers:    helpers,
-  schema:     schema
-};
+  helpers: helpers,
+  schema: schema
+}
